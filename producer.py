e304dd14e9849466da83ee58d3112f22caf931d1
Polina Silina <gomik3112002@gmail.com>
2026-03-01 17:03:59 +0300

    correction

--- producer.py
+++ producer.py
@@ -1,16 +1,27 @@
+import psycopg2
+from kafka import KafkaProducer
+import json
+import time
+
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-users = ["alice", "bob", "carol", "dave"]
+conn = psycopg2.connect(
+    dbname="test_db", user="admin", password="admin", host="localhost", port=5432
+)
+cursor = conn.cursor()
 
-while True:
+cursor.execute("SELECT username, event_type, extract(epoch FROM event_time) FROM user_logins")
+rows = cursor.fetchall()
+
+for row in rows:
     data = {
-        "user": random.choice(users),
-        "event": "login",
-        "timestamp": time.time()
+        "user": row[0],
+        "event": row[1],
+        "timestamp": float(row[2])  # преобразуем Decimal → float
     }
     producer.send("user_events", value=data)
     print("Sent:", data)
-    time.sleep(1)
+    time.sleep(0.5)
